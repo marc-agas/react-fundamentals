2c11c87d89136970463b56594223f57c28c4d9f7
Marc Agas <marc.agas@cheetahdigital.com>
2022-01-18 14:53:31 +0800

    Complete exercises.

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -12,7 +12,7 @@ import '../box-styles.css'
 
 // 🐨 add a style prop to each of them as well so their background color
 // matches what the text says it should be as well as `fontStyle: 'italic'`
-const smallBox = <div>small lightblue box</div>
+const smallBox = <div className="box">small lightblue box</div>
 const mediumBox = <div>medium pink box</div>
 const largeBox = <div>large orange box</div>
 
